0e2becec2e9b17ab9631c46aa45d39b4cba49b2d
jarannis <cbarnes@elitchgardens.com>
2016-07-18 22:36:34 -0600

    Mysql database connection initialization.

--- resource/mysql.init.php
+++ resource/mysql.init.php
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-//require_once "/config/mysql.conf.php";
+require_once getcwd() . "/config/mysql.conf.php";
 echo getcwd() . "\n";
 
 $link = mysqli_connect(
