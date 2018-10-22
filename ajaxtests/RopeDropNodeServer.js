21ae578f87a03586e9454c27b24637a4c804f3f2
Christian Barnes <jarannis@gmail.com>
2018-10-22 15:38:16 -0600

    Faffing around with URL parsing

--- ajaxtests/RopeDropNodeServer.js
+++ ajaxtests/RopeDropNodeServer.js
@@ -1,6 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res){
 	var url = req.url;
+	var query = url.parse(url, true).query;
+	var txt = q.a + " " + q.b;
 	res.write(url);
+	res.write("/n");
+	res.write(txt);
 	res.end();
 }).listen(8080);
