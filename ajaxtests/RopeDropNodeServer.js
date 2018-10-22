f51db3a8f489f3d22237b850fa78a808ce7ebce2
Christian Barnes <jarannis@gmail.com>
2018-10-22 15:39:48 -0600

    Faffing Around

--- ajaxtests/RopeDropNodeServer.js
+++ ajaxtests/RopeDropNodeServer.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res){
 	var url = req.url;
-	var query = url.parse(url, true).query;
+	var q = url.parse(req.url, true).query;
 	var txt = q.a + " " + q.b;
 	res.write(url);
 	res.write("/n");
