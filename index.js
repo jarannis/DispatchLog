a20ac2d71056d1be7416a2a2a4ef735317d34608
Christian Barnes <jarannis@gmail.com>
2018-11-07 13:50:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-index.js
+/* Homepage Generator for RopeDrop V 0.0.1
+Created by Christian Barnes
+
+Homepage loads Session Management first, checks for a valid session, checks session against unique "Last Session" code from DB.
+Sessions expire after 24 hours, and a new session code must be generated.
+
+*/
+require('./resource/mysql/mysql.init.js');
